979e7402f30f77c8ca22d2523f8ba0e58fc60dd9
Parker Hutchinson <hutchinsonparker@gmail.com>
2019-09-27 13:29:10 -0700

    centering the eye

--- src/components/04_ecosystems/capabilities.tsx
+++ src/components/04_ecosystems/capabilities.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import styled from 'styled-components';
+import Section from '../03_organisms/section';
 const eye = require('~/static/images/svg/eye.svg');
 
+const StyledEye = styled.div`
+  grid-column: 6 / span 18;
+  img{
+    display: block;
+    width: 100%;
+  }   
+`;
 
 const Capabilites = () => {
   return (
-    <section>
-      <img src={eye} alt=""/>
-    </section>
+    <Section label="">
+      <StyledEye>
+        <img src={eye} alt=""/>
+      </StyledEye>
+    </Section>
   )
 }
 
